b3028a3923e0e1da8b90a787e8b57be1e3aee17b
miguepoloc <miguepoloc@gmail.com>
2023-03-28 13:24:56 -0500

    :whale: Docker compose funcionando

---
+++ settings/base.py
@@ -0,0 +1,220 @@
+import cloudinary
+import os
+from pathlib import Path
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Cloudinary para configuración de fotos
+cloudinary.config(
+    cloud_name="djw2ks8ek",
+    api_key="245669729881259",
+    api_secret="FUcSyfHSnr39Rv8zSiQ2VzEUOyo",
+    secure=True
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-h82t0qs_s_&fop)=lw8@k$+d)x0i%)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '0.0.0.0']
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+    '0.0.0.0'
+]
+
+# Application definition
+
+INSTALLED_APPS = [
+    "corsheaders",
+    "rest_framework",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    "drf_spectacular",
+    "api",
+    "authentication",
+    "core"
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'cognied.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'cognied.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default1': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'digitalmente',
+        'USER': 'admin',
+        'PASSWORD': 'Contrasena1!',
+        'HOST': '137.184.232.104',
+        'PORT': '3306',
+    },
+    'default3': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'encuesta4',
+        'USER': 'admin',
+        'PASSWORD': 'Contrasena1!',
+        'HOST': '137.184.232.104',
+        'PORT': '3306',
+    },
+    'default_mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'encuesta4',
+        'USER': 'admin',
+        'PASSWORD': 'Contrasena1!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kidsbpep',
+        'USER': 'kidsbpep',
+        'PASSWORD': 'Da_X0HObSCVPd9uHyG9RA--JVubROEar',
+        # 'HOST': 'kashin.db.elephantsql.com',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'es-co'
+
+TIME_ZONE = 'America/Bogota'
+
+USE_I18N = True
+
+USE_TZ = True
+
+USE_L10N = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+LOGIN_REDIRECT_URL = '/'
+
+# Django Rest-Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication.backends.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+# Activate Django-Heroku.
+# django_heroku.settings(locals())
+
+
+SPECTACULAR_SETTINGS = {
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "persistAuthorization": True,
+        "displayOperationId": True,
+    },
+    'TITLE': "DigitalMente API",
+    'VERSION': 'v1',
+    'DESCRIPTION': "API para el proyecto de DigitalMente",
+    "CONTACT": {"email": "miguelpoloac@unimagdalena.edu.co"},
+    "LICENSE": {"name": "Cognied"},
+    'SERVE_PERMISSIONS': ['rest_framework.permissions.AllowAny'],
+    "SERVE_AUTHENTICATION": ['authentication.backends.JWTAuthentication']
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'no-reply.digitalmente@outlook.com'
+EMAIL_HOST_PASSWORD = 'digitalM3nte'
